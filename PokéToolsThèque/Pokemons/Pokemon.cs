4b7e9258cc5573e2b01684018f6889b6fe500f5a
psycox <lizee.etu@gmail.com>
2025-07-16 02:04:00 +0200

    v9 randompicker

--- PokéToolsThèque/Pokemons/Pokemon.cs
+++ PokéToolsThèque/Pokemons/Pokemon.cs
@@ -36,10 +36,7 @@ namespace PokéToolsThèque.Pokemons
         public string SmogonTier { get; set; }
         public bool RandomSet { get; set; } 
 
-
-
         public Tiers? Tiers { get; set; }
-
 
         //Export
         public string? SelectedItem { get; set; }
@@ -47,5 +44,46 @@ namespace PokéToolsThèque.Pokemons
         public string? SelectedNature { get; set; }
         public Dictionary<string, int> EVs { get; set; } = new();
         public List<string> SelectedMoves { get; set; } = new();
+
+        public Pokemon()
+        {
+            
+        }
+
+        public Pokemon(Pokemon source)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+
+            Dex = source.Dex;
+            Name = source.Name;
+            Form = source.Form;
+            Type1 = source.Type1;
+            Type2 = source.Type2;
+            Total = source.Total;
+            HP = source.HP;
+            Attack = source.Attack;
+            Defense = source.Defense;
+            SpAtk = source.SpAtk;
+            SpDef = source.SpDef;
+            Speed = source.Speed;
+            Gen = source.Gen;
+
+            Nom = source.Nom;
+            Ether = source.Ether;
+            Psycox = source.Psycox;
+
+            IconUrl = source.IconUrl;
+            PokeApiIdentifier = source.PokeApiIdentifier;
+
+            Moves = source.Moves != null ? new List<Move>(source.Moves) : new List<Move>();
+            Abilities = source.Abilities != null ? new List<Ability>(source.Abilities) : new List<Ability>();
+
+            IsFullyEvolved = source.IsFullyEvolved;
+            Category = source.Category;
+            SmogonTier = source.SmogonTier;
+            RandomSet = source.RandomSet;
+
+            Tiers = source.Tiers;
+        }
     }
 }
